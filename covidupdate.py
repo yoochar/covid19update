b3233c52e60c1bb962bfc055507653ce497bbd95
yoochar <yoochar@gmail.com>
2020-03-24 19:45:48 +0700

    upload

--- covidupdate.py
+++ covidupdate.py
@@ -1,3 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 url="https://ddc.moph.go.th/viralpneumonia/"
+data=requests.get(url)
+#print(data.status_code)
+#print(data.text)
+
+soup=BeautifulSoup(data.text,'html.parser')
+
+result =soup.find_all("td",{"class":"popup_num txt"})
+for item in result:
+    print(item)
